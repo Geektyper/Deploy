a675e6e4223df22266c67e7fff6ae6e034d83f70
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-10-05 09:37:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import subprocess
 from config import *
+
+clone_cmd = f'git clone https://{GITHUB_ACCESS_TOKEN}@github.com/{GITHUB_USERNAME}/{GITHUB_REPO_NAME}'
+cd_cmd = f'cd {GITHUB_REPO_NAME}'
+
+if __name__ == '__main__':
+    subprocess.run(clone_cmd.split())
+    subprocess.run(cd_cmd.split())
+    subprocess.run(BUILD_COMMAND.split())
+    subprocess.run(RUN_COMMAND.split())
