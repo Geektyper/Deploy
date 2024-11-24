fb6eff94a8ad983df9dda76b0c1308d899cbb99d
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-11-24 16:10:04 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from config import *
-
-clone_cmd = f'git clone https://{GITHUB_ACCESS_TOKEN}@github.com/{GITHUB_USERNAME}/{GITHUB_REPO_NAME}'
-
-if __name__ == '__main__':
-    subprocess.run(clone_cmd.split())
-    subprocess.run(BUILD_COMMAND.split(), cwd=GITHUB_REPO_NAME)
-    subprocess.run(RUN_COMMAND.split(), cwd=GITHUB_REPO_NAME)
