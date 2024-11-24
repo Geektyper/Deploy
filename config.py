cf8425a969b16a6d2cada396e2d3435896ba89ad
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-11-24 16:09:56 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-GITHUB_USERNAME = '' # Github Username of Repo's Owner.
-GITHUB_REPO_NAME = '' # Repo Name, Ex:- If the url is 'https://github.com/username/repo_name', Enter the repo_name.
-GITHUB_ACCESS_TOKEN = '' # # Your Github Acess Token.
-
-BUILD_COMMAND = '' # For Installing Requirements, Ex:- 'pip install -r requirements.txt'.
-RUN_COMMAND = '' # The Main Command, Ex:- 'python3 main.py'.
